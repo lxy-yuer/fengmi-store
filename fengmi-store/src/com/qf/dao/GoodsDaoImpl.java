fb124c573182562102d7683ca3207dedeb9c1638
_4444x_ <_4444x_@xxxx>
2020-08-18 17:17:54 +0800

    _4444x_ update

--- fengmi-store/src/com/qf/dao/GoodsDaoImpl.java
+++ fengmi-store/src/com/qf/dao/GoodsDaoImpl.java
@@ -1,5 +1,7 @@
 package com.qf.dao;
 
+import java.util.List;
+
 import com.qf.entity.Goods;
 import com.qf.util.DBUtils;
 
@@ -14,8 +16,38 @@ public class GoodsDaoImpl implements GoodsDao{
 	
 	@Override
 	public int addGoods(Goods good) {
-		String sql = "insert into t_goods(username,password,img) values(?,?,?)";
-		int i = dbUtil.updateData(sql, user.getUsername(),user.getPassword(),user.getImg());
+		String sql = "insert into t_goods(name,price,pubdate,tid,intro,pricture,star) values(?,?,?,?,?,?,?)";
+		int i = dbUtil.updateData(sql, good.getName(),good.getPrice(),good.getPubdate(),good.getTid(),good.getIntro(),good.getPicture(),good.getStar());
 		return i;
+	}
+
+	@Override
+	public int deleteGoods(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateGoods(Goods good) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Goods getGoodsById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Goods getGoodsByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Goods> getGoodsList() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
